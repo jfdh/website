648d1c369bde2c7acab5b925d0387bd5df535b14
Xavier Damman <xdamman@gmail.com>
2017-01-24 21:45:45 +0100

    added a paragraph on the homepage with JF to explain what we do, refactored link to newsletter

--- pages/index.jsx
+++ pages/index.jsx
@@ -61,10 +61,20 @@ class Index extends React.Component {
     return (
       <DocumentTitle title={`${config.blogTitle}`}>
       <div className="content">
+        <h2>Why don't you do it?</h2>
         <iframe className="youtube" width="560" height="315" src="https://www.youtube.com/embed/wVTx3OorJtc" frameBorder="0" allowFullscreen></iframe>
         <ul className="posts">
           {pageLinks}
         </ul>
+        <p>We all have something to contribute to make Brussels a better city. But it's still too hard to get things done.</p>
+        <p>BrusselsTogether lets you create a virtual association so that you can get started in no time and focus on what you want to do to improve Brussels. No paperwork needed.<br />
+        <a href="/services/">Find out more</a>.</p>
+        <p>
+        We curate a <b>monthly newsletter</b> with the best initiatives happening in Brussels.<br /><a href="http://brusselstogether.us15.list-manage.com/subscribe?u=bc4a7d8ae7c38b1bb4ae32404&id=08f5b0187e">Sign up for the newsletter</a>.
+        </p>
+        <p>
+        We also host a <b>monthly meetup</b> where we showcase 3 great initiatives and give you the opportunity to share yours.<br /><a href="https://meetup.com/BrusselsTogether">Join our Meetup Group</a>.
+        </p>
         <h2>Join us</h2>
         <div dangerouslySetInnerHTML={{__html: contactPage}}/>
       </div>
